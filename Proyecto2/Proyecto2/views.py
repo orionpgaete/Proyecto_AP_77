7a0754709cda5a422c125b2609624e5b88ed3c89
orionpgaete <pedro.gaete@pgcomputer.cl>
2022-09-05 21:42:01 -0300

    uso clase Persona

--- Proyecto2/Proyecto2/views.py
+++ Proyecto2/Proyecto2/views.py
@@ -2,12 +2,19 @@ import datetime
 from django.http import HttpResponse
 from django.template import Template, Context
 
+class Persona(object):
+    def __init__(self, nombre, apellido):
+        self.nombre=nombre
+        self.apellido=apellido
+
 def plantilla(request): # Primera Vista
 
-    nombre = "Pedro"   #usando parametros
-    apellido = "Gaete"
+    #nombre = "Pedro"   #usando parametros
+    #apellido = "Gaete"
 
     ahora = datetime.datetime.now() #trae fecha de ahora
+
+    p1 = Persona("Profesor Pedro", "Gaete")
 
     doc_externo = open("D:/1. Clases INACAP/2. Clases/2.31 Programacion Back-End/Proyectos-AP-77/Proyecto_AP_77/proyecto2/Proyecto2/web/miplantilla.html")
 
@@ -15,7 +22,7 @@ def plantilla(request): # Primera Vista
 
     doc_externo.close()
 
-    context = Context({"nom_persona":nombre, "apel_persona":apellido, "fecha":ahora})
+    context = Context({"nom_persona":p1.nombre, "apel_persona":p1.apellido, "fecha":ahora})
 
     documento = plantilla.render(context)
     

5367c284a1c2909bc65861113e59756fe0bcc2bf
orionpgaete <pedro.gaete@pgcomputer.cl>
2022-10-03 22:05:27 -0300

    CRUD & Formularios

--- Proyecto4/Proyecto4/urls.py
+++ Proyecto4/Proyecto4/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from gestionPedido_APP import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('busqueda/', views.busqueda),
+    path('buscar/', views.buscar),
 ]
 
 
--- Proyecto4/gestionPedido_APP/views.py
+++ Proyecto4/gestionPedido_APP/views.py
@@ -1,3 +1,15 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def busqueda(request):
+    return render(request, "busqueda_producto.html")
+
+def buscar(request):
+    if request.GET ["prd"]:
+        mensaje = "Articulo buscado: %r" %request.GET["prd"]
+    else:
+        mensaje="No has introducido nada"
+        
+    return HttpResponse(mensaje)
